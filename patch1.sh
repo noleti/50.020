56ea7fddd004d5831b42f44e1bb2ea6110762277
noleti <noleti@gmx.de>
2014-10-08 11:48:10 +0800

    Minor change to fix PSQL setup

--- patch1.sh
+++ patch1.sh
@@ -14,8 +14,11 @@ sudo tar xvzf info.tar.gz -C /var/lib/dpkg/info/
 sudo echo "" > /var/lib/dpkg/info/postgresql-9.3.prerm
 sudo echo "" > /var/lib/dpkg/info/postgresql-client-9.3.prerm
 
+sudo rm -r /var/log/postgresql
+sudo rm -r /var/lib/postgresql
+
 # Now, we should be able to remove postgre and ruby
-sudo apt-get remove postgresql-client-9.3 postgresql-9.3 postgresql-common postgresql-client-common libpq-dev libpq5 pgadmin3 pgagent postgresql-9.3 postgresql libruby1.9.1 libruby1.9.1-dbg
+sudo apt-get purge postgresql-client-9.3 postgresql-9.3 postgresql-common postgresql-client-common libpq-dev libpq5 pgadmin3 pgagent postgresql-9.3 postgresql libruby1.9.1 libruby1.9.1-dbg
 
 # lets clean up the gems and other garbage
 sudo rm -rf /var/lib/gems /usr/bin/pg_config.libpq-dev /usr/lib/ruby /etc/postgresql-common/user_clusters
@@ -31,4 +34,7 @@ bundle install
 sudo rm -rf /home/student/.msf4
 
 # last but not least, we need to reboot for some reason to get psql to work
-sudo reboot
+#sudo reboot
+
+# After reboot, execute the msf_install again
+# cd Downloads && bash msf_install.sh

6d9e37a8aec2be27742de8da6e4623d27b76e329
noleti <noleti@gmx.de>
2014-10-08 11:04:41 +0800

    verified

--- patch1.sh
+++ patch1.sh
@@ -9,7 +9,10 @@
 
 # First, we need to fix broken uninstaller for postgre
 sudo tar xvzf info.tar.gz -C /var/lib/dpkg/info/
-# TODO: check if +x has to be set for some of these
+
+# "fix" some removal scripts
+sudo echo "" > /var/lib/dpkg/info/postgresql-9.3.prerm
+sudo echo "" > /var/lib/dpkg/info/postgresql-client-9.3.prerm
 
 # Now, we should be able to remove postgre and ruby
 sudo apt-get remove postgresql-client-9.3 postgresql-9.3 postgresql-common postgresql-client-common libpq-dev libpq5 pgadmin3 pgagent postgresql-9.3 postgresql libruby1.9.1 libruby1.9.1-dbg
@@ -18,14 +21,14 @@ sudo apt-get remove postgresql-client-9.3 postgresql-9.3 postgresql-common postg
 sudo rm -rf /var/lib/gems /usr/bin/pg_config.libpq-dev /usr/lib/ruby /etc/postgresql-common/user_clusters
 
 # Start the install process again
-bash msf_install.sh
+bash msf_install.sh -i
 
 # Now, re-run the bundle install as normal user
-cd /usr/local/share/metasploit-framework-git/
-sudo rm msfconsole
+cd /usr/local/share/metasploit-framework/
+sudo rm msfconsole msfrpcd
 sudo git pull
 bundle install
 sudo rm -rf /home/student/.msf4
 
-# last but not least, we need to reboot for some reason to get pgsql to work
+# last but not least, we need to reboot for some reason to get psql to work
 sudo reboot
